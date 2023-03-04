2a79fa58e0bb2ced3b59465abbf3d36a26caa8c5
Anant Gabhane <gabhaneanant90@gmail.com>
2023-03-04 11:02:25 +0530

    Get-Superhero and Supervillen

---
+++ index.js
@@ -0,0 +1,12 @@
+var superheroes = require("superheroes");
+var supervillains = require("supervillains");
+
+var mysuperheroes = superheroes.random();
+var mysupervillain = supervillains.random();
+
+console.log(mysuperheroes);
+console.log(mysupervillain);
+
+
+
+
